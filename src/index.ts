040824f32eee74742e57d4b713d882f41a2991c5
Dandrezz <diego@gmail.com>
2024-03-13 08:09:55 -0500

    Push changes to remote repository

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { Hono } from 'hono'
 import { actorRoutes } from './routers/actor';
 import { videoRoutes } from './routers/video';
 import { Bindings } from 'hono/types';
+import { cors } from 'hono/cors'
 
 const app = new Hono<{ Bindings: Bindings }>()
 
+app.use('/api/*', cors())
 app.route('/api/actor', actorRoutes );
 app.route('/api/video', videoRoutes );
 
